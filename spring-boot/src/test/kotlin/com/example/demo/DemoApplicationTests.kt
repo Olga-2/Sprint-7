0805f13fca04bfe5422848ef0e624b740b47ccbc
Olga <o2-2010@mail.ru>
2021-11-06 00:32:56 +0300

    Spring-boot

--- spring-boot/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ spring-boot/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,42 @@
 package com.example.demo
 
+import com.example.demo.controller.Controller
+import com.example.demo.persistance.Entity
+import com.example.demo.persistance.EntityRepository
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
-@SpringBootTest
+@WebMvcTest(Controller::class)
 class DemoApplicationTests {
 
+	@Autowired
+	private lateinit var mockMvc: MockMvc
 	@Test
-	fun contextLoads() {
+	fun `shoud greeting get status OK`() {
+		val result = mockMvc.perform (MockMvcRequestBuilders.get("/"))
+		result
+			.andExpect(MockMvcResultMatchers.status().isOk)
+			.andExpect(MockMvcResultMatchers.content().string("Hello, my frend!"))
 	}
+}
 
+@DataJpaTest
+class EntityRepositoryTest {
+	@Autowired
+	private lateinit var repository: EntityRepository
+	@Test
+	fun `find entitybyid`() {
+		//given
+		val savedEntity = repository.save(Entity(name = "Ivan"))
+		//when
+		val foundEntity = repository.findById(savedEntity.id!!)
+		//then
+		Assertions.assertTrue(savedEntity == foundEntity.get())
+	}
 }
