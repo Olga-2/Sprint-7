985c3ded1459a446ac725da7e341582f9cb70cc3
Olga <o2-2010@mail.ru>
2021-11-19 08:51:22 +0300

    spring-data-security + TestRestTemplate

--- spring-security/src/test/kotlin/org/spring/mvc/springsecurity/SpringSecurityApplicationTests.kt
+++ spring-security/src/test/kotlin/org/spring/mvc/springsecurity/SpringSecurityApplicationTests.kt
@@ -1,7 +1,5 @@
 package org.spring.mvc.springsecurity
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -12,145 +10,191 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.core.ParameterizedTypeReference
-import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.http.*
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class SpringSecurityApplicationTests {
 
-	@Autowired
-	private lateinit var mockMvc: MockMvc
+    @Autowired
+    private lateinit var mockMvc: MockMvc
 
-	@Autowired
-	private lateinit var objectMapper: ObjectMapper
-
-
-	@Test
-    @WithMockUser(username="user",roles=["APP"])
+    @Test
+    @WithMockUser(username = "user", roles = ["APP"])
     fun testAddAndOneRecordView() {
 
-		mockMvc.perform(
-			MockMvcRequestBuilders.post("/app/add")
-				.accept(MediaType.APPLICATION_JSON_VALUE)
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.flashAttr("Address", Address("Oscar", "Tisovaya st.")))
-			.andDo { println() }
-			.andExpect ( status().is3xxRedirection )
-			.andExpect ( redirectedUrl("/app/list?fullName") )
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/app/add")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .flashAttr("Address", Address("Oscar", "Tisovaya st."))
+        )
+            .andDo { println() }
+            .andExpect(status().is3xxRedirection)
+            .andExpect(redirectedUrl("/app/list?fullName"))
 
-		mockMvc.perform(
-			MockMvcRequestBuilders.get("/app/list/Oscar")
-				.accept(MediaType.APPLICATION_JSON_VALUE)
-				.contentType(MediaType.APPLICATION_JSON_VALUE))
-			.andDo { println() }
-			.andExpect ( status().isOk )
-			.andExpect ( content().string(containsString("Oscar")) )
-			.andExpect ( content().string(containsString("Tisovaya")) )
-	}
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/app/list/Oscar")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        )
+            .andDo { println() }
+            .andExpect(status().isOk)
+            .andExpect(content().string(containsString("Oscar")))
+            .andExpect(content().string(containsString("Tisovaya")))
+    }
 
-	@Test
-    @WithMockUser(username="bob",roles=["API"])
-    fun testList()
-	{
-		mockMvc.perform(
-			MockMvcRequestBuilders.get("/app/list")
-				.param("fullName","")
-		)
-			.andDo { println() }
-			.andExpect ( status().isOk() )
-			.andExpect ( view().name("addressList") )
-			.andExpect ( content().string(containsString("Name")) )
-			.andExpect ( content().string(containsString("Address")) )
-	}
+    @Test
+    @WithMockUser(username = "bob", roles = ["API"])
+    fun testList() {
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/app/list")
+                .param("fullName", "")
+        )
+            .andDo { println() }
+            .andExpect(status().isOk())
+            .andExpect(view().name("addressList"))
+            .andExpect(content().string(containsString("Name")))
+            .andExpect(content().string(containsString("Address")))
+    }
 
-	@Test
-    @WithMockUser(username="bob",roles=["API"])
+    @Test
+    @WithMockUser(username = "bob", roles = ["API"])
     fun testView() {
 
-		mockMvc.perform(
-			MockMvcRequestBuilders.get("/app/5/view")
-				//.cookie(cookie)
-		)
-			.andDo { println() }
-			.andExpect ( status().isOk() )
-			.andExpect ( content().string(containsString("Address not exist!")) )
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/app/5/view")
+            //.cookie(cookie)
+        )
+            .andDo { println() }
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Address not exist!")))
 
-	}
+    }
 
-	@Test
-    @WithMockUser(username="user",roles=["APP"])
+    @Test
+    @WithMockUser(username = "user", roles = ["APP"])
     fun testFind() {
 
-		mockMvc.perform(
-			MockMvcRequestBuilders.get("/app/list/Ivanov Ivan")
-			//	.cookie(cookie)
-		)
-			.andDo { println() }
-			.andExpect (status().isOk() )
-			.andExpect ( view().name("addressFind") )
-			.andExpect ( content().string(containsString("Edit")) )
-			.andExpect ( content().string(containsString("View")) )
-			.andExpect ( content().string(containsString("Delete")) )
-		}
-	}
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/app/list/Ivanov Ivan")
+            //	.cookie(cookie)
+        )
+            .andDo { println() }
+            .andExpect(status().isOk())
+            .andExpect(view().name("addressFind"))
+            .andExpect(content().string(containsString("Edit")))
+            .andExpect(content().string(containsString("View")))
+            .andExpect(content().string(containsString("Delete")))
+    }
+}
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class RestApplicationTest {
 
-	@LocalServerPort
-	private var port: Int = 0
+    @LocalServerPort
+    private var port: Int = 0
 
-	@Autowired
-	private lateinit var restTemplate: TestRestTemplate
+    @Autowired
+    private lateinit var restTemplate: TestRestTemplate
 
-	private fun url(s: String): String {
-		return "http://localhost:${port}/${s}"
+    private fun url(s: String): String {
+        return "http://localhost:${port}/${s}"
+    }
+
+    @Test
+    fun getList() {
+
+        val cookie = getCookieForUser("bob", "Bob123", url("/login"))
+        val headers = HttpHeaders()
+        headers.add("Cookie", cookie)
+
+        val response = restTemplate
+            .exchange(
+                url("/api/list"), HttpMethod.GET, HttpEntity<String>(headers),
+                object : ParameterizedTypeReference<List<Address>>() {}
+            )
+        val address = response.getBody()
+
+        assertNotNull(response)
+        assertEquals(200, response.statusCodeValue)
+        assertTrue(address!!.size == 4)
+    }
+
+
+    private fun getCookieForUser(username: String, password: String, loginUrl: String): String? {
+        val form: MultiValueMap<String, String> = LinkedMultiValueMap()
+        form.set("username", username);
+        form.set("password", password);
+        val loginResponse = restTemplate.postForEntity(
+            loginUrl,
+            HttpEntity(form, HttpHeaders()), String::class.java
+        )
+        var jSessionId: String? = null
+        for (cookie in loginResponse.headers.get("Set-Cookie")!!)
+            if (cookie.startsWith("JSESSIONID"))
+                jSessionId = cookie
+
+            return jSessionId
+    }
+
+
+	@Test
+	fun getViewId() {
+
+		val cookie = getCookieForUser("bob", "Bob123", url("/login"))
+		val headers = HttpHeaders()
+		headers.add("Cookie", cookie)
+
+		val resp = restTemplate.exchange<Address>(
+			url("/api/1/view"), HttpMethod.GET,  HttpEntity<Address>(headers),
+			Address::class.java
+
+		)
+
+		val address = resp.body!!
+		assertNotNull(resp)
+		assertEquals(200, resp.statusCodeValue)
+		assertEquals("Petrov Ivan", address.fullName)
+		assertEquals("Nikitina,  14", address.address)
 	}
 
-//	@Test
-//	@WithMockUser(username = "bob", roles = ["API"])
-//	fun getList() {
-//
-//		class ListOfAddress : ParameterizedTypeReference<List<Address>>()
-//
-//		val response=
-//			restTemplate.exchange<List<Address>>(url("/api/list"),
-//			HttpMethod.GET, null, String.javaClass)
-//		val address= JsonPar(response.body!!)
-//
-//		assertNotNull(response)
-//		assertEquals(200, response.statusCodeValue)
-//		assertTrue(address!!.size == 4)
-//	}
+    @Test
+    fun testAddAndOneRecordView() {
 
-//	@Test
-//	fun getViewId() {
-//
-//		var resp = restTemplate.postForEntity(
-//			url("/api/login"),
-//			HttpEntity<Any>(User("Bob", "Bob123")), User::class.java
-//		)
-//
-//		val headers = HttpHeaders()
-//		headers.add("Cookie", resp.headers.getFirst("Set-Cookie"))
-//		class ListOfAddress : ParameterizedTypeReference<List<Address>>() {}
-//
-//		val resp1 = restTemplate.exchange(
-//			url("/api/1/view"), HttpMethod.GET,
-//			HttpEntity<String>(headers), Address::class.java
-//		)
-//		assertNotNull(resp1)
-//		assertEquals(200, resp1.statusCodeValue)
-//		assertEquals("Petrov Ivan", resp1.body!!.fullName)
-//		assertEquals("Nikitina,  14", resp1.body!!.address)
-//	}
-//
+        val cookie = getCookieForUser("bob", "Bob123", url("/login"))
+        val headers = HttpHeaders()
+        headers.add("Cookie", cookie)
+
+        var resp = restTemplate.exchange<Address>(
+            url("/api/add"), HttpMethod.PUT,
+            HttpEntity<Address>(Address("Oscar", "Tisovaya st."), headers),
+            Address::class.java
+        )
+
+        assertNotNull(resp)
+        assertEquals(200, resp.statusCodeValue)
+
+        resp = restTemplate.exchange<Address>(
+            url("/api/{id}/view"), HttpMethod.GET, HttpEntity<Address>(headers),
+            Address::class.java, 4
+        )
+
+        val address = resp.body!!
+        assertNotNull(resp)
+        assertEquals(200, resp.statusCodeValue)
+        assertEquals("Oscar", address.fullName)
+        assertEquals("Tisovaya st.", address.address)
+    }
 
 }
+
